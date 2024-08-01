203e6eb07aef597ff77157cf7652c3e605504519
Łukasz Sieniec <lukasz.sieniec@gmail.com>
2024-08-01 23:04:50 +0200

    Add basic page for Dashboard

---
+++ lib/features/dashboard/dashboard_page.dart
@@ -0,0 +1,11 @@
+import 'package:dinero/features/dashboard/dashboard_body.dart';
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DashboardBody();
+  }
+}
