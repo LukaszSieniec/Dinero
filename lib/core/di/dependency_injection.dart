4922a45555662bbe481ee7226dc5dec3c9ce2e6e
Łukasz Sieniec <lukasz.sieniec@gmail.com>
2024-08-01 22:31:04 +0200

    Add configuration for dependency injection

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,27 @@
+import 'package:dinero/common/utils/app_environment.dart';
+import 'package:dinero/core/di/dependency_injection.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+@injectableInit
+void configureDependencies({
+  required AppEnvironment environment,
+}) {
+  getIt.registerSingleton<AppEnvironment>(environment);
+  getIt.init();
+}
+
+T inject<T extends Object>({
+  String? instanceName,
+  dynamic param1,
+  dynamic param2,
+  Type? type,
+}) =>
+    getIt.get<T>(
+      instanceName: instanceName,
+      param1: param1,
+      param2: param2,
+      type: type,
+    );
