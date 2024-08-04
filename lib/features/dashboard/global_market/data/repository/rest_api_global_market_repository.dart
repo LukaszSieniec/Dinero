07c75b18f90f3aaee8228bbdf3125685139c3040
Łukasz Sieniec <lukasz.sieniec@gmail.com>
2024-08-05 00:26:51 +0200

    Provide next version of repository

--- lib/features/dashboard/global_market/data/repository/rest_api_global_market_repository.dart
+++ lib/features/dashboard/global_market/data/repository/rest_api_global_market_repository.dart
@@ -1,20 +1,27 @@
 import 'package:dinero/common/utils/result.dart';
-import 'package:dinero/features/dashboard/global_market/data/network/dto/global_market_response.dart';
+import 'package:dinero/features/dashboard/global_market/data/mapper/global_market_auto_mapper.dart';
 import 'package:dinero/features/dashboard/global_market/data/network/global_market_api.dart';
+import 'package:dinero/features/dashboard/global_market/domain/model/global_market.dart';
 import 'package:dinero/features/dashboard/global_market/domain/repository/global_market_repository.dart';
 import 'package:dio/dio.dart';
 
 class RestApiGlobalMarketRepository implements GlobalMarketRepository {
   final GlobalMarketApi _globalMarketApi;
+  final GlobalMarketAutoMapper _autoMapper;
 
-  const RestApiGlobalMarketRepository(this._globalMarketApi);
+  const RestApiGlobalMarketRepository(
+    this._globalMarketApi,
+    this._autoMapper,
+  );
 
   @override
-  Future<Result<GlobalMarketResponse, DioException>> fetchGlobalMarket() async {
+  Future<Result<GlobalMarket, DioException>> fetchGlobalMarket() async {
     try {
       final response = await _globalMarketApi.fetchGlobalMarket();
 
-      return Result.success(data: response);
+      final globalMarket = _autoMapper.convert(response);
+
+      return Result.success(data: globalMarket);
     } on DioException catch (error) {
       return Result.failure(error: error);
     }
--- lib/features/dashboard/global_market/global_market_module.dart
+++ lib/features/dashboard/global_market/global_market_module.dart
@@ -1,4 +1,5 @@
 import 'package:dinero/core/di/dependency_injection.dart';
+import 'package:dinero/features/dashboard/global_market/data/mapper/global_market_auto_mapper.dart';
 import 'package:dinero/features/dashboard/global_market/data/network/global_market_api.dart';
 import 'package:dinero/features/dashboard/global_market/data/repository/rest_api_global_market_repository.dart';
 import 'package:dinero/features/dashboard/global_market/domain/repository/global_market_repository.dart';
@@ -9,6 +10,9 @@ import 'package:injectable/injectable.dart';
 abstract class GlobalMarketModule {
   @LazySingleton(as: GlobalMarketRepository)
   RestApiGlobalMarketRepository get globalMarketRepository;
+
+  @lazySingleton
+  GlobalMarketAutoMapper get autoMapper => GlobalMarketAutoMapper();
 
   @lazySingleton
   GlobalMarketApi get globalMarketApi => GlobalMarketApi(inject<Dio>());
