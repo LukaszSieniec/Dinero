182d90767b437f103cd4b45d63fb5fcb353e28a8
Łukasz Sieniec <lukasz.sieniec@gmail.com>
2024-08-15 03:13:10 +0200

    Provide dispose method for dashboard

--- lib/features/dashboard/dashboard_body.dart
+++ lib/features/dashboard/dashboard_body.dart
@@ -30,6 +30,12 @@ class _DashboardBodyState extends State<DashboardBody>
   }
 
   @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
