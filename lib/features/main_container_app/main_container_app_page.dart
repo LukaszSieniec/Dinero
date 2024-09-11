ac911fca70b3c5fdde57f6aff54f0755d1481028
Łukasz Sieniec <lukasz.sieniec@gmail.com>
2024-09-11 15:54:42 +0200

    Remove body for MainContainerApp

--- lib/features/main_container_app/main_container_app_page.dart
+++ lib/features/main_container_app/main_container_app_page.dart
@@ -1,4 +1,4 @@
-import 'package:dinero/features/main_container_app/main_container_app_body.dart';
+import 'package:dinero/features/main_container_app/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,6 +12,9 @@ class MainContainerAppPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainContainerAppBody(navigationShell: navigationShell);
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavBar(navigationShell: navigationShell),
+    );
   }
 }
