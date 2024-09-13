f81ecac96621692972329448cceae94942093967
Łukasz Sieniec <lukasz.sieniec@gmail.com>
2024-09-13 17:08:54 +0200

    Add ValueNotifier to manage button visibility

--- lib/features/main_container_app/main_container_app_body.dart
+++ lib/features/main_container_app/main_container_app_body.dart
@@ -4,7 +4,6 @@ import 'package:dinero/features/dashboard/cryptocurrencies/presentation/bloc_com
 import 'package:dinero/features/main_container_app/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 // Allows you to access the InnerController.
@@ -23,13 +22,13 @@ class MainContainerAppBody extends StatefulWidget {
 }
 
 class _MainContainerAppBodyState extends State<MainContainerAppBody> {
-  late bool isVisibleButton;
+  late ValueNotifier<bool> isVisibleButton;
 
   @override
   void initState() {
     super.initState();
     // Initially, the FloatingActionButton is not visible.
-    isVisibleButton = false;
+    isVisibleButton = ValueNotifier(false);
 
     // A method that allows specific code to be executed
     // after the rendering phase is completed.
@@ -45,10 +44,8 @@ class _MainContainerAppBodyState extends State<MainContainerAppBody> {
             final maxScrollExtent = innerController.position.maxScrollExtent;
             final currentScrollOffset = innerController.offset;
 
-            setState(
-              () => isVisibleButton =
-                  currentScrollOffset > (maxScrollExtent * 0.10),
-            );
+            isVisibleButton.value =
+                currentScrollOffset > (maxScrollExtent * 0.10);
 
             if (currentScrollOffset >= (maxScrollExtent * 0.95)) {
               context
@@ -70,22 +67,25 @@ class _MainContainerAppBodyState extends State<MainContainerAppBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: isVisibleButton
-          ? FloatingActionButton(
-              child: const Icon(
-                Icons.keyboard_arrow_up_outlined,
-                color: AppPalette.white,
-              ),
-              onPressed: () => nestedScrollViewGlobalKey
-                  .currentState?.innerController
-                  .animateTo(
-                // TODO: We need to verify this in the future.
-                -1.0,
-                duration: Durations.long1,
-                curve: Curves.ease,
-              ),
-            )
-          : null,
+      floatingActionButton: ValueListenableBuilder(
+        valueListenable: isVisibleButton,
+        builder: (context, isVisibleButton, child) => isVisibleButton
+            ? FloatingActionButton(
+                child: const Icon(
+                  Icons.keyboard_arrow_up_outlined,
+                  color: AppPalette.white,
+                ),
+                // TODO: We need to verify this in the future (offset value).
+                onPressed: () => nestedScrollViewGlobalKey
+                    .currentState?.innerController
+                    .animateTo(
+                  -1.0,
+                  duration: Durations.long1,
+                  curve: Curves.ease,
+                ),
+              )
+            : const SizedBox(),
+      ),
       body: widget.navigationShell,
       bottomNavigationBar:
           BottomNavBar(navigationShell: widget.navigationShell),
